9b28fb6f8c90bf357c7385863eedbd88b20ca1ba
sidi-125 <sidra.saqib712@gmail.com>
2025-04-16 13:00:50 +0200

    Added Projects page

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
 import React from 'react';
-import Header from './components/Header'; 
-import Footer from './components/Footer'; 
-import Sidebar from './components/Sidebar.js';
-import HeroSection from './components/Hero.js';
-import SkillsSection from './components/Skills.js';
-import EducationSection from './components/Education.js';
-import ProjectsSection from './components/Project.js';
-import CertificationSection from './components/Certification.js';
-import ContactSection from './components/Contact.js';
-
-
-import './App.css'; // make sure styling is correct
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Projects from './Pages/Projects'; 
 
 function App() {
   return (
-    <div className="app-container">
-      <Sidebar />
-      <Header />
-
-      <main className="main-content">
-
-        <section id="home"><HeroSection /></section>
-        <section id="education"><EducationSection /></section>
-        <section id="certification"><CertificationSection /></section>
-        <section id="projects"><ProjectsSection /></section>
-        <section id="skills"><SkillsSection /></section>
-        <section id="contact"><ContactSection /></section>
-      </main>
-      <Footer />
-
-    </div>
-    
+    <Router>
+      <Routes>
+        <Route path="/Projects" element={<Projects />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
