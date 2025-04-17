c2f0e5e64cbde4968e9c6d139d51ae83c75f26ed
sidi-125 <sidra.saqib712@gmail.com>
2025-04-17 09:04:27 +0200

    Remove contact name

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from "./Pages/About";
-import Contact from './Pages/Contact';
 import './App.css';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/About" element={<About />} />
         
-        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
