56f38dc9dca9179dc20e94f20a6aa93346c3358e
sidi-125 <sidra.saqib712@gmail.com>
2025-04-18 12:30:44 +0200

    Applied stash@{1} for Contact page changes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import About from './Pages/About';
 import Contact from './Pages/Contact';
 import Skills from './Pages/Skills';
 import Projects from './Pages/Projects';
+<<<<<<< Updated upstream
 import Certifications from './Pages/Certifications';
 import Education from './Pages/Education';
 import './App.css';
 
+=======
+import './App.css';
+import CertificationSection from './Pages/Certifications';
+>>>>>>> Stashed changes
 
 
 function App() {
@@ -20,8 +25,12 @@ function App() {
         <Route path="/Contact" element={<Contact />} />
         <Route path="/Skills" element={<Skills />} />
         <Route path="/Projects" element={<Projects />} />
+<<<<<<< Updated upstream
         <Route path="/Certifications" element={<Certifications />} />
         <Route path="/Education" element={<Education />} />
+=======
+        <Route path="/Certifications" element={<CertificationSection />} />
+>>>>>>> Stashed changes
 
 
 
