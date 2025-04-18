cef823487168ad4908c59b2e32b4a7231dfe7d29
sidi-125 <sidra.saqib712@gmail.com>
2025-04-18 11:50:34 +0200

    Added Contact page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Contact from './Pages/Contact';
 import Skills from './Pages/Skills';
 import Projects from './Pages/Projects';
 import './App.css';
+import Certifications from './Pages/Certifications';
 
 
 function App() {
@@ -17,6 +18,8 @@ function App() {
         <Route path="/Contact" element={<Contact />} />
         <Route path="/Skills" element={<Skills />} />
         <Route path="/Projects" element={<Projects />} />
+        <Route path="/Certifications" element={<Certifications />} />
+
 
 
 
