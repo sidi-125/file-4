a5be4a2191deee94e766ed59ddd721ea2fdbd1c7
sidi-125 <sidra.saqib712@gmail.com>
2025-04-18 11:32:19 +0200

    Added Skills  page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home'; 
 import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Skills from './Pages/Skills';
+
 import './App.css';
 
 function App() {
@@ -10,6 +13,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Skills" element={<Skills />} />
+
+
+        
 
 
       </Routes>
