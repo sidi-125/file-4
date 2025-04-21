8843d548cca3857fa9f179703c2944686f4a168c
sidi-125 <sidra.saqib712@gmail.com>
2025-04-21 15:16:53 +0200

    Add some data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 // src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './Layout'; // Layout component
-import About from './Pages/About';
-import Contact from './Pages/Contact';
-import Skills from './Pages/Skills';
-import Projects from './Pages/Projects';
-import Certifications from './Pages/Certifications';
-import Education from './Pages/Education';
+import Home from './Pages/Home'; 
 import './App.css';
 
 
@@ -17,19 +11,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/Skills" element={<Skills />} />
-        <Route path="/Projects" element={<Projects />} />
-        <Route path="/Certifications" element={<CertificationSection />} />
-
-
-
-
-
-        
-
-
       </Routes>
     </Router>
   );
