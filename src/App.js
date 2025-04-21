bdfa5a2dffdd83b015637154e9cc28b50faeed91
sidi-125 <sidra.saqib712@gmail.com>
2025-04-21 15:19:06 +0200

    Save current changes in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home'; 
+import About from './Pages/About';
 import './App.css';
 
 
@@ -11,6 +12,9 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+
+
       </Routes>
     </Router>
   );
