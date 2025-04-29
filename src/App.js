628c010fd63691a89e040812023c8f677d499567
sidi-125 <sidra.saqib712@gmail.com>
2025-04-29 14:13:59 +0200

    Resolved stash@{3} conflict in App.js and restored changes

--- src/App.js
+++ src/App.js
@@ -14,13 +14,36 @@ function AppGeneralized() {
     { name: 'Sara', age: 25, city: 'Karachi' },
     { name: 'Zain', age: 20, city: 'Islamabad' },
   ];
+import GeneralizedTable from './Pages/Generalize';
+
+
+const columns = [
+  { label: 'Name', field: 'name' },
+  { label: 'Age', field: 'age' },
+  { label: 'City', field: 'city' },
+];
+
+const data = [
+  { name: 'Ali', age: 22, city: 'Lahore' },
+  { name: 'Sara', age: 25, city: 'Karachi' },
+  { name: 'Zain', age: 20, city: 'Islamabad' },
+];
 
   return (
     <div style={{ padding: '20px' }}>
+<<<<<<< Updated upstream
       <h2>Generalized Table (Fixed Columns)</h2>
+=======
+      <h2>Generalized Table</h2>
+>>>>>>> Stashed changes
       <GeneralizedTable columns={columns} data={data} />
     </div>
   );
 }
 
+<<<<<<< Updated upstream
 export default AppGeneralized;
+=======
+export default GeneralizedTable;
+
+>>>>>>> Stashed changes
